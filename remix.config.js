4b1dd1aaa2d7408b8f880055f979c942536b4369
Pinche Madre <pinche.madre@putaloca.co>
2024-04-24 10:55:56 +0200

    Add remix-flat-routes

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,11 @@
+const { flatRoutes } = require('remix-flat-routes')
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
   serverModuleFormat: "cjs",
+  routes: async defineRoutes => {
+    return flatRoutes('routes', defineRoutes)
+  },
 };
