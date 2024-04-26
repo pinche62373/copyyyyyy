e2460480086ba47fc82c1f4f1a2c7a848d196905
Pinche Madre <pinche.madre@putaloca.co>
2024-04-26 15:39:52 +0200

    Add font Inter Variable

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import {
 import { type IStaticMethods } from "preline/preline";
 import { useEffect } from "react";
 
+import "@fontsource-variable/inter/wght.css";
 import { getUser } from "~/session.server";
 import stylesheet from "~/tailwind.css";
 
