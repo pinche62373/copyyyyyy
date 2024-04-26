c4c5270f8cd26778445bed6f76aa8ae7271af709
Pinche Madre <pinche.madre@putaloca.co>
2024-04-26 13:24:26 +0200

    Update admin menu

--- app/routes/admin+/index.tsx
+++ app/routes/admin+/index.tsx
@@ -56,7 +56,7 @@ export default function AdminIndexPage() {
             </svg>
             <div className="sm:order-1 grow space-y-1">
               <h2 className="sm:mb-3 text-sm text-stone-500 dark:text-neutral-400">
-                Performers
+                Actors
               </h2>
               <p className="text-lg md:text-xl font-semibold text-stone-800 dark:text-neutral-200">
                 29
