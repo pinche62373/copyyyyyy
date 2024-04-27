b4a24474d942e424829f6d19e3d8ee44619ec7e6
Pinche Madre <pinche.madre@putaloca.co>
2024-04-27 13:51:53 +0200

    Cleanup THead

--- app/components/tanstack-table/THead.tsx
+++ app/components/tanstack-table/THead.tsx
@@ -2,11 +2,7 @@ import { flexRender } from "@tanstack/react-table";
 
 import useTable from "./useTable";
 
-interface IProps<T> {
-  isSortable?: boolean;
-}
-
-const THead = <T,>({ isSortable }: IProps<T>) => {
+const THead = () => {
   const table = useTable();
 
   if (!table) return null;
@@ -16,47 +12,39 @@ const THead = <T,>({ isSortable }: IProps<T>) => {
         {table.getHeaderGroups().map((x) => {
           return x.headers.map((header) => {
             return (
-              <th key={header.id} scope="col" className={"min-w-[20px]" + (header.column.getCanSort() ? "" : "cursor-default !important")}>
+              <th key={header.id} scope="col" className="min-w-[20px]">
                 {header.isPlaceholder ? null : (
                   <div className="flex">
-                    <div className="hs-dropdown relative inline-flex w-full">
-                      <span
-                        onClick={
-                          isSortable
-                            ? header.column.getToggleSortingHandler()
-                            : undefined
-                        }
+                    <div className="relative inline-flex w-full">
+                      <button
+                        type="button"
+                        className="px-5 py-2.5 text-start w-full flex items-center gap-x-1 text-sm font-normal text-gray-500 focus:outline-none focus:bg-gray-100 dark:text-neutral-500 dark:focus:bg-neutral-700"
+                        onClick={header.column.getToggleSortingHandler()}
                       >
-                        <button
-                          id="hs-pro-ptpn"
-                          type="button"
-                          className="px-5 py-2.5 text-start w-full flex items-center gap-x-1 text-sm font-normal text-gray-500 focus:outline-none focus:bg-gray-100 dark:text-neutral-500 dark:focus:bg-neutral-700"
-                        >
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
 
-                          {/* Conditional Sort Icon */}
-                          {header.column.getCanSort() && (
-                            <svg
-                              className="flex-shrink-0 size-3.5"
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeWidth="2"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            >
-                              <path d="m7 15 5 5 5-5" />
-                              <path d="m7 9 5-5 5 5" />
-                            </svg>
-                          )}
-                        </button>
-                      </span>
+                        {/* Conditional Sort Icon */}
+                        {header.column.getCanSort() && (
+                          <svg
+                            className="flex-shrink-0 size-3.5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <path d="m7 15 5 5 5-5" />
+                            <path d="m7 9 5-5 5 5" />
+                          </svg>
+                        )}
+                      </button>
                     </div>
                   </div>
                 )}
