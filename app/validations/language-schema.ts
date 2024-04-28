ce7121f08611953355604f7142b162850ba4b443
Pinche Madre <pinche.madre@putaloca.co>
2024-04-28 14:20:47 +0200

    Zod regex for language name

--- app/validations/language-schema.ts
+++ app/validations/language-schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
 export const languageSchema = z.object({
-  name: z.string({ required_error: 'Language name is required' }),
+  name: z
+    .string({ required_error: 'Language name is required' })
+    .regex(/^\p{Lu}\p{Ll}+( \p{Lu}\p{Ll}+)*$/, { message: 'Language names must start with a capital letter, separated by a single whitespace.' })
 });
