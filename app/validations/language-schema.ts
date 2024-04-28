f2b1377ed3fd338a87c50c2d8986696e8da052c5
Pinche Madre <pinche.madre@putaloca.co>
2024-04-28 14:54:59 +0200

    Harden latin words regex

--- app/validations/language-schema.ts
+++ app/validations/language-schema.ts
@@ -1,7 +1,11 @@
+//
+// Regex used here => Capitalized Latin words, separated by spaces, no diacritics (https://regex101.com/r/lTm7Df/1)
+// Variation => capitalized Unicode words, separated by spaces, allows diacritics (https://regex101.com/r/iY7iJ6/2)
+//
 import { z } from "zod";
 
 export const languageSchema = z.object({
   name: z
     .string({ required_error: 'Language name is required' })
-    .regex(/^\p{Lu}\p{Ll}+( \p{Lu}\p{Ll}+)*$/, { message: 'Language names must start with a capital letter, separated by a single whitespace.' })
+    .regex(/^[A-Z][a-z]+( [A-Z][a-z]+)*$/, { message: 'This field only allows capitalized Latin words, separated by spaces.' })
 });
