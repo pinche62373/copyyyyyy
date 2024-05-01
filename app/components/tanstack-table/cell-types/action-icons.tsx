ba27e62ccb8dbd62ce761429136ec8cdb8af775c
Pinche Madre <pinche.madre@putaloca.co>
2024-05-01 14:03:37 +0200

    Enable table icon edit

--- app/components/tanstack-table/cell-types/action-icons.tsx
+++ app/components/tanstack-table/cell-types/action-icons.tsx
@@ -1,6 +1,7 @@
-import { Form } from "@remix-run/react";
+import { Form, NavLink } from "@remix-run/react";
 import { CellContext } from "@tanstack/react-table";
 
+import { AdminFormFieldHidden } from "#app/components/admin/form";
 import { IconEdit } from "#app/components/icons/edit";
 import { IconTrash } from "#app/components/icons/trash";
 
@@ -14,12 +15,11 @@ export const getCellActionIcons = (info: CellContext<any, string>) => {
     <>
       {/* Edit Button */}
       <div className="inline-flex items-center -space-x-px">
-        <button
-          className="size-8 inline-flex justify-center items-center gap-x-2 font-medium rounded-s-lg border border-stone-200 bg-white text-stone-800 shadow-sm hover:bg-stone-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-stone-50 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-          onClick={() => alert("Edit")}
-        >
-          <IconEdit className="flex-shrink-0 size-3.5" />
-        </button>
+        <NavLink to={info.row.original.id}>
+          <button className="size-8 inline-flex justify-center items-center gap-x-2 font-medium rounded-s-lg border border-stone-200 bg-white text-stone-800 shadow-sm hover:bg-stone-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-stone-50 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+            <IconEdit className="flex-shrink-0 size-3.5" />
+          </button>
+        </NavLink>
       </div>
       {/* End Edit Button */}
 
@@ -34,8 +34,8 @@ export const getCellActionIcons = (info: CellContext<any, string>) => {
             }
           }}
         >
-          <input type="hidden" name="intent" value="delete" />
-          <input type="hidden" name="id" value={info.row.original.id} />
+          <AdminFormFieldHidden name="intent" value="delete" />
+          <AdminFormFieldHidden name="id" value={info.row.original.id} />
 
           <button className="size-8 inline-flex justify-center items-center gap-x-2 font-medium rounded-r-lg border border-stone-200 bg-white text-stone-800 shadow-sm hover:bg-stone-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-stone-50 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700  hover:fill-red-500">
             <IconTrash className="flex-shrink-0 size-3.5" />
