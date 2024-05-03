2cc75b6090eb1f036382a6e876b6f4f6d8ad42be
Pinche Madre <pinche.madre@putaloca.co>
2024-05-03 15:16:50 +0200

    Revert to default prisma client

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,36 +1,9 @@
-import { remember } from '@epic-web/remember'
-import { PrismaClient } from '@prisma/client'
-import chalk from 'chalk'
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = remember('prisma', () => {
-	// NOTE: if you change anything in this function you'll need to restart
-	// the dev server to see your changes.
+import { singleton } from "./singleton.server";
 
-	// Feel free to change this log threshold to something that makes sense for you
-	const logThreshold = 20
+// Hard-code a unique key, so we can look up the client when this module gets re-imported
+const prisma = singleton("prisma", () => new PrismaClient());
+prisma.$connect();
 
-	const client = new PrismaClient({
-		log: [
-			{ level: 'query', emit: 'event' },
-			{ level: 'error', emit: 'stdout' },
-			{ level: 'warn', emit: 'stdout' },
-		],
-	})
-	client.$on('query', async e => {
-		if (e.duration < logThreshold) return
-		const color =
-			e.duration < logThreshold * 1.1
-				? 'green'
-				: e.duration < logThreshold * 1.2
-					? 'blue'
-					: e.duration < logThreshold * 1.3
-						? 'yellow'
-						: e.duration < logThreshold * 1.4
-							? 'redBright'
-							: 'red'
-		const dur = chalk[color](`${e.duration}ms`)
-		console.info(`prisma:query - ${dur} - ${e.query}`)
-	})
-	client.$connect()
-	return client
-})
+export { prisma };
