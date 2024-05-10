5a8f61450e418af1d7075a01166f9da882c33f61
Pinche Madre <pinche.madre@putaloca.co>
2024-05-10 11:02:22 +0200

    Replace movies icon

--- app/components/admin/admin-sidebar.tsx
+++ app/components/admin/admin-sidebar.tsx
@@ -2,8 +2,9 @@ import { NavLink } from "@remix-run/react";
 
 import { IconDog } from "#app/components/icons/icon-dog";
 import { IconHome } from "#app/components/icons/icon-home";
+import { IconMovie } from "#app/components/icons/icon-movie";
 import { IconUsers } from "#app/components/icons/icon-users";
-import  * as Constants from "#app/utils/constants"
+import * as Constants from "#app/utils/constants";
 
 export function AdminSidebar() {
   return (
@@ -135,7 +136,7 @@ export function AdminSidebar() {
                   type="button"
                   className="hs-accordion-toggle hs-accordion-active:bg-gray-100 w-full text-start flex gap-x-3 py-2 px-3 text-sm text-gray-800 rounded-lg hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:bg-neutral-700 focus:outline-none focus:bg-gray-100 dark:hover:bg-neutral-700 dark:text-neutral-300 dark:focus:bg-neutral-700"
                 >
-                  <IconDog className="flex-shrink-0 mt-0.5 size-4" /> 
+                  <IconDog className="flex-shrink-0 mt-0.5 size-4" />
                   Animals
                   <svg
                     className="hs-accordion-active:-rotate-180 flex-shrink-0 mt-1 size-3.5 ms-auto transition"
@@ -222,31 +223,9 @@ export function AdminSidebar() {
                   type="button"
                   className="hs-accordion-toggle hs-accordion-active:bg-gray-100 w-full text-start flex gap-x-3 py-2 px-3 text-sm text-gray-800 rounded-lg hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:bg-neutral-700 focus:outline-none focus:bg-gray-100 dark:hover:bg-neutral-700 dark:text-neutral-300 dark:focus:bg-neutral-700"
                 >
-                  <svg
-                    className="flex-shrink-0 mt-0.5 size-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <circle cx="18" cy="15" r="3" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M10 15H6a4 4 0 0 0-4 4v2" />
-                    <path d="m21.7 16.4-.9-.3" />
-                    <path d="m15.2 13.9-.9-.3" />
-                    <path d="m16.6 18.7.3-.9" />
-                    <path d="m19.1 12.2.3-.9" />
-                    <path d="m19.6 18.7-.4-1" />
-                    <path d="m16.8 12.3-.4-1" />
-                    <path d="m14.3 16.6 1-.4" />
-                    <path d="m20.7 13.8 1-.4" />
-                  </svg>
+                  <IconMovie className="flex-shrink-0 size-5" />
                   Movies
+                  {/* Chevron Icon */}
                   <svg
                     className="hs-accordion-active:-rotate-180 flex-shrink-0 mt-1 size-3.5 ms-auto transition"
                     xmlns="http://www.w3.org/2000/svg"
@@ -261,6 +240,7 @@ export function AdminSidebar() {
                   >
                     <path d="m6 9 6 6 6-6" />
                   </svg>
+                  {/* End Chevron Icon */}
                 </button>
 
                 <div
