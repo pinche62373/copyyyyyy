f76aaacaaaaadafa20b1fc10c614cc77942adfa8
Pinche Madre <pinche.madre@putaloca.co>
2024-05-16 16:04:20 +0200

    Harden logout

--- app/routes/auth+/logout.tsx
+++ app/routes/auth+/logout.tsx
@@ -1,9 +1,31 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 
 import { authenticator } from "#app/utils/auth.server";
+import { prisma } from "#app/utils/db.server";
+import { sessionCookie } from "#app/utils/session.server";
 
-export const action = async ({ request }: ActionFunctionArgs) =>
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const sessionId = await sessionCookie.parse(request.headers.get("Cookie"));
+
+  // delete orphaned client-side session cookie not existing in database to prevent breaking remix-auth
+  if (sessionId) {
+    const databaseSessionExists = await prisma.session.findUnique({
+      where: { id: sessionId },
+    });
+
+    if (!databaseSessionExists) {
+      return json(null, {
+        headers: {
+          "Set-Cookie": await sessionCookie.serialize("", {
+            maxAge: 0,
+          }),
+        },
+      });
+    }
+  }
+
   await authenticator.logout(request, { redirectTo: "/" });
+};
 
 export const loader = async () => redirect("/");
