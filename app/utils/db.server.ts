17e7008390411c7fe7e96e36d3f78a4aedfd2f18
Pinche Madre <pinche.madre@putaloca.co>
2024-05-16 14:13:37 +0200

    Cs

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -4,6 +4,7 @@ import { singleton } from "../singleton.server";
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
 const prisma = singleton("prisma", () => new PrismaClient());
+
 prisma.$connect();
 
 export { prisma };
