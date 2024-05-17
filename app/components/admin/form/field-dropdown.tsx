387a0b4123aa3e6097877dd7fa90f6340352183c
Pinche Madre <pinche.madre@putaloca.co>
2024-05-17 11:40:22 +0200

    Fix dropdown validation

--- app/components/admin/form/field-dropdown.tsx
+++ app/components/admin/form/field-dropdown.tsx
@@ -14,7 +14,7 @@ export const AdminFormFieldDropdown = ({
   fields,
   options,
 }: PropTypes) => {
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState<string>();
 
   const changeSelected = (optionId: string) => {
     setSelected(optionId);
@@ -35,13 +35,14 @@ export const AdminFormFieldDropdown = ({
         {/* Col Name Select*/}
         <div className="sm:col-span-10 md:col-span-10 lg:col-span-10 xl:col-span-11">
           <select
+            id="regionId"
             name="regionId"
             onChange={(event) => changeSelected(event.target.value)}
             value={selected}
             className="py-2 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
           >
             {/* Options */}
-            <option>Select a region...</option>
+            <option disabled selected>Select a region...</option>
 
             {options.map((option, i: number) => {
               return (
