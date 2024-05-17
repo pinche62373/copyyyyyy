1f27a96f8691ede9ebe1943a971c6b1c9bed5af3
Pinche Madre <pinche.madre@putaloca.co>
2024-05-17 12:56:06 +0200

    Fix PropTypes and defaultValue for dropdown

--- app/components/admin/form/field-dropdown.tsx
+++ app/components/admin/form/field-dropdown.tsx
@@ -5,7 +5,10 @@ interface PropTypes {
   fieldName: string;
   label: string;
   fields: ReturnType<FormMetadata["getFieldset"]>;
-  options: any;
+  options: {
+    id: string;
+    name: string;
+  }[];
 }
 
 export const AdminFormFieldDropdown = ({
@@ -42,7 +45,9 @@ export const AdminFormFieldDropdown = ({
             className="py-2 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
           >
             {/* Options */}
-            <option disabled selected>Select a region...</option>
+            <option disabled selected defaultValue={undefined}>
+              Select a region...
+            </option>
 
             {options.map((option, i: number) => {
               return (
