f605d5af1916f64282b899da534302d0ffddd689
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-05-30 15:49:17 +0200

    Use Button for page title

--- app/components/admin/admin-page-title.tsx
+++ app/components/admin/admin-page-title.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "@remix-run/react";
+import { Button } from "#app/components/admin/button";
 
 type PropTypes =
   | {
@@ -61,29 +61,7 @@ export const AdminPageTitle = ({
 
           {/* Conditional Add Button  */}
           {buttonText && (
-            <NavLink
-              type="button"
-              to={buttonTarget}
-              className="py-2 px-3 inline-flex justify-center items-center gap-x-2 text-start bg-blue-600 border border-blue-600 text-white text-sm font-medium rounded-lg shadow-sm align-middle hover:bg-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-300 dark:focus:ring-blue-500"
-              // className="py-2 px-3 inline-flex justify-center items-center gap-x-2 text-start bg-blue-600 border border-blue-600 text-white text-sm font-medium rounded-lg shadow-sm align-middle hover:bg-blue-700 focus:outline-none focus:ring-1 focus:ring-blue-300 dark:focus:ring-blue-500"
-            >
-              <svg
-                className="hidden sm:block flex-shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M5 12h14" />
-                <path d="M12 5v14" />
-              </svg>
-              {buttonText}
-            </NavLink>
+            <Button type="button" text={buttonText} link={buttonTarget} />
           )}
           {/* End Conditional Add Button  */}
         </div>
