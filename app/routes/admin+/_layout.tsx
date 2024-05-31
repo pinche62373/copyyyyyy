d84d1997a27feaa3091bee3e79187263706331a6
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-05-31 08:04:40 +0200

    Protect admin route by role

--- app/routes/admin+/_layout.tsx
+++ app/routes/admin+/_layout.tsx
@@ -7,6 +7,7 @@ import { AdminSidebar } from "#app/components/admin/admin-sidebar";
 import adminStyleSheet from "#app/styles/admin.css";
 import { authenticator } from "#app/utils/auth.server";
 import { AUTH_LOGIN_ROUTE } from "#app/utils/constants";
+import { requireUserWithRole } from "#app/utils/rbac.server";
 
 // import styles for the admin route
 export const links: LinksFunction = () => [
@@ -20,6 +21,8 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticator.isAuthenticated(request, {
     failureRedirect: AUTH_LOGIN_ROUTE + `?returnTo=${url.pathname}`,
   });
+
+  await requireUserWithRole(request, ["admin", "moderator"]);
 
   return null;
 };
