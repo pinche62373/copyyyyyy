a4ffe66eefa4e6d9303c63edaa106873cebb9ed9
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-06-22 20:06:18 +0200

    Improve region schema

--- app/validations/region-schema.ts
+++ app/validations/region-schema.ts
@@ -1,18 +1,21 @@
 //
-// Regex used here => Capitalized Latin words, separated by spaces, no diacritics (https://regex101.com/r/lTm7Df/1)
+// Regex used for `name` => Capitalized Latin words, separated by spaces, no diacritics (https://regex101.com/r/lTm7Df/1)
 // Variation => capitalized Unicode words, separated by spaces, allows diacritics (https://regex101.com/r/iY7iJ6/2)
 //
 import { z } from "zod";
 
 export const regionSchemaFull = z.object({
-  id: z.string(),
+  id: z.string().cuid2(),
   name: z
     .string({ required_error: "Region name is required" })
     .regex(/^[A-Z][a-z]+( [A-Z][a-z]+)*$/, {
       message:
         "This field only allows capitalized Latin words, separated by spaces.",
     }),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
   createdBy: z.string(),
+  updatedBy: z.string(),
 });
 
 export const regionSchemaCreateForm = regionSchemaFull.pick({
