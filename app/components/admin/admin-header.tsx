0718de5bb890ead4f8512fd06348db5e9184c26f
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-07-29 13:18:51 +0200

    Fix hydration errors

--- app/components/admin/admin-header.tsx
+++ app/components/admin/admin-header.tsx
@@ -1,4 +1,5 @@
 import { Form } from "@remix-run/react";
+import { useHydrated } from "remix-utils/use-hydrated";
 
 import { AdminBreadcrumbs } from "#app/components/admin/admin-breadcrumbs";
 import { IconCog } from "#app/components/icons/icon-cog";
@@ -12,6 +13,8 @@ import { useUser } from "#app/utils/user";
 import { DarkModeToggler } from "../darkmode-toggler";
 
 export const AdminHeader = () => {
+  const isHydrated = useHydrated();
+
   const user = useUser();
 
   return (
@@ -70,71 +73,73 @@ export const AdminHeader = () => {
             <div className="h-[38px]">
               <DarkModeToggler className="size-[38px] inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent text-gray-500 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" />
 
-              {/* Account Dropdown */}
-              <div className="hs-dropdown relative inline-flex [--strategy:absolute] [--auto-close:inside] [--placement:bottom-right]">
-                <button
-                  id="@@id"
-                  type="button"
-                  className="size-[38px] inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent text-gray-500 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                  title={user.email}
-                >
-                  <IconUser />
-                </button>
+              {/* HYDRATION - Account Dropdown */}
+              {isHydrated ? (
+                <div className="hs-dropdown relative inline-flex [--strategy:absolute] [--auto-close:inside] [--placement:bottom-right]">
+                  <button
+                    id="@@id"
+                    type="button"
+                    className="size-[38px] inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent text-gray-500 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                    title={user.email}
+                  >
+                    <IconUser />
+                  </button>
 
-                {/* Account Dropdown Items */}
-                <div
-                  className="hs-dropdown-menu hs-dropdown-open:opacity-100 w-60 transition-[opacity,margin] duration opacity-0 hidden z-10 bg-white rounded-xl shadow-[0_10px_40px_10px_rgba(0,0,0,0.08)] dark:bg-neutral-900 dark:shadow-[0_10px_40px_10px_rgba(0,0,0,0.2)]"
-                  aria-labelledby="@@id"
-                >
-                  {/* Account Dropdown Items Group 1 */}
-                  <div className="p-1">
-                    {/* My Likes */}
-                    <a
-                      className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                      href="/"
-                    >
-                      <IconHeart />
-                      My Likes
-                    </a>
-
-                    {/* My Collection */}
-                    <a
-                      className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                      href="/"
-                    >
-                      <IconFolder />
-                      My Collection
-                    </a>
-
-                    {/* Settings */}
-                    <a
-                      className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                      href="/"
-                    >
-                      <IconCog />
-                      Settings
-                    </a>
-                  </div>
-                  {/* End Account Dropdown Items Group 1 */}
-
-                  {/* Account Dropdown Items Group 2 */}
-                  <div className="p-1 border-t border-gray-200 dark:border-neutral-800">
-                    <Form action={AUTH_LOGOUT_ROUTE} method="post">
-                      <button
-                        type="submit"
-                        className="flex mt-0.5 gap-x-3 py-2 px-3 w-full rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                        data-hs-overlay="#hs-pro-dasadam"
+                  {/* Account Dropdown Items */}
+                  <div
+                    className="hs-dropdown-menu hs-dropdown-open:opacity-100 w-60 transition-[opacity,margin] duration opacity-0 hidden z-10 bg-white rounded-xl shadow-[0_10px_40px_10px_rgba(0,0,0,0.08)] dark:bg-neutral-900 dark:shadow-[0_10px_40px_10px_rgba(0,0,0,0.2)]"
+                    aria-labelledby="@@id"
+                  >
+                    {/* Account Dropdown Items Group 1 */}
+                    <div className="p-1">
+                      {/* My Likes */}
+                      <a
+                        className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+                        href="/"
                       >
-                        <IconLogout />
-                        Sign Out
-                      </button>
-                    </Form>
+                        <IconHeart />
+                        My Likes
+                      </a>
+
+                      {/* My Collection */}
+                      <a
+                        className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+                        href="/"
+                      >
+                        <IconFolder />
+                        My Collection
+                      </a>
+
+                      {/* Settings */}
+                      <a
+                        className="flex items-center gap-x-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+                        href="/"
+                      >
+                        <IconCog />
+                        Settings
+                      </a>
+                    </div>
+                    {/* End Account Dropdown Items Group 1 */}
+
+                    {/* Account Dropdown Items Group 2 */}
+                    <div className="p-1 border-t border-gray-200 dark:border-neutral-800">
+                      <Form action={AUTH_LOGOUT_ROUTE} method="post">
+                        <button
+                          type="submit"
+                          className="flex mt-0.5 gap-x-3 py-2 px-3 w-full rounded-lg text-sm text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+                          data-hs-overlay="#hs-pro-dasadam"
+                        >
+                          <IconLogout />
+                          Sign Out
+                        </button>
+                      </Form>
+                    </div>
+                    {/* End Account Dropdown Items Group 2 */}
                   </div>
-                  {/* End Account Dropdown Items Group 2 */}
+                  {/* End Account Dropdown Items */}
                 </div>
-                {/* End Account Dropdown Items */}
-              </div>
-              {/* End Account Dropdown */}
+              ) : null}
+              {/* End HYDRATION - Account Dropdown */}
             </div>
             {/* End Icon List */}
           </div>
--- app/components/tanstack-table/TableBar.tsx
+++ app/components/tanstack-table/TableBar.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from "react";
+import { useHydrated } from "remix-utils/use-hydrated";
 
 import { cn } from "#app/utils/misc";
-
 interface IProps {
   className?: string;
 }
@@ -11,15 +11,21 @@ export const TableBar = ({
   children,
   ...rest
 }: PropsWithChildren<IProps>) => {
+  const isHydrated = useHydrated();
+
   return (
-    <div
-      className={cn(
-        "px-2 grid md:grid-cols-2 gap-y-2 md:gap-y-0 md:gap-x-5",
-        className,
-      )}
-      {...rest}
-    >
-      {children}
-    </div>
+    <>
+      {isHydrated ? (
+        <div
+          className={cn(
+            "px-2 grid md:grid-cols-2 gap-y-2 md:gap-y-0 md:gap-x-5",
+            className,
+          )}
+          {...rest}
+        >
+          {children}
+        </div>
+      ) : null}
+    </>
   );
 };
