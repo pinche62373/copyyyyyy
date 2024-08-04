9b9b790e80c428835c76b2434299b1688fd479e8
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-08-04 17:11:31 +0200

    Allow null for permission.updatedAt

--- prisma/migrations/20240804150832_init/migration.sql
+++ prisma/migrations/20240804150832_init/migration.sql
@@ -41,7 +41,7 @@ CREATE TABLE "Permission" (
     "scope" TEXT NOT NULL,
     "description" TEXT NOT NULL DEFAULT '',
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "updatedAt" DATETIME
 );
 
 -- CreateTable
