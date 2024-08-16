e088f4a7d276316412fc1b82259d8911524455f1
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-08-16 16:38:45 +0200

    Fix darkmode-toggler hydration issue

--- app/components/darkmode-toggler.tsx
+++ app/components/darkmode-toggler.tsx
@@ -1,23 +1,30 @@
 import { Theme, useTheme } from "remix-themes";
+import { useHydrated } from "remix-utils/use-hydrated";
 
 import { IconDarkModeDisable } from "#app/components/icons/icon-darkmode-disable";
 import { IconDarkModeEnable } from "#app/components/icons/icon-darkmode-enable";
 
 export function DarkModeToggler({ ...props }) {
+  const isHydrated = useHydrated();
+
   const [theme, setTheme] = useTheme();
 
   return (
-    <button
-      aria-label="Toggle Theme"
-      type="button"
-      onClick={() =>
-        setTheme((prev) => (prev === Theme.DARK ? Theme.LIGHT : Theme.DARK))
-      }
-      {...props}
-    >
-      {theme === Theme.LIGHT && <IconDarkModeEnable />}
+    <>
+      {isHydrated ? (
+        <button
+          aria-label="Toggle Theme"
+          type="button"
+          onClick={() =>
+            setTheme((prev) => (prev === Theme.DARK ? Theme.LIGHT : Theme.DARK))
+          }
+          {...props}
+        >
+          {theme === Theme.LIGHT && <IconDarkModeEnable />}
 
-      {theme === Theme.DARK && <IconDarkModeDisable />}
-    </button>
+          {theme === Theme.DARK && <IconDarkModeDisable />}
+        </button>
+      ) : null}
+    </>
   );
 }
