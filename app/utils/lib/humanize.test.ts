0a210d19fb2f8865e4898831fdebf1ceeb0f0f85
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-11-01 10:57:33 +0100

    Package bumps

---
+++ app/utils/lib/humanize.test.ts
@@ -0,0 +1,11 @@
+import { humanize } from "./humanize";
+
+describe("Ensure humanize():", function () {
+  it("returns single word in PascalCase", () => {
+    expect(humanize("country")).toEqual("Country");
+  });
+
+  it("returns multi word in PascalCase", () => {
+    expect(humanize("countryTable")).toEqual("CountryTable");
+  });
+});
