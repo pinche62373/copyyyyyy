ec3fd6c1d61e467d2cfc8edfe5a2c0b876537a7a
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-11-11 10:22:25 +0100

    Refactor Button

--- app/components/shared/button.tsx
+++ app/components/shared/button.tsx
@@ -21,13 +21,23 @@ export function Button({
   ...rest
 }: ButtonProps) {
   const tvButton = tv({
-    base: "inline-flex items-center justify-center whitespace-nowrap rounded-lg border px-3 py-1.5 text-start align-middle font-medium shadow-sm focus:outline-none focus:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+    base: cn(
+      "inline-flex items-center justify-center whitespace-nowrap rounded-md",
+      "px-3 py-1.5 text-start align-middle text-sm font-medium",
+      "focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50"
+    ),
     variants: {
       role: {
-        primary:
-          "gap-x-2 border-blue-800 bg-blue-800 text-sm text-white hover:bg-blue-700 focus:ring-blue-700 dark:hover:border-blue-700 dark:focus:ring-blue-700",
-        secondary:
-          "border-gray-200 bg-white text-sm text-gray-800 hover:bg-gray-100 focus:bg-gray-50 focus:ring-gray-50 dark:border-border-foreground dark:bg-input dark:text-secondary-foreground dark:hover:bg-input dark:hover:text-primary-foreground dark:focus:bg-neutral-700 dark:focus:ring-neutral-800"
+        primary: cn(
+          "bg-[#4361ee] text-white hover:bg-[#3c57d6]",
+          "dark:bg-[#4361ee]/70 dark:text-neutral-300 dark:hover:bg-[#4361ee]/60 dark:hover:text-neutral-300/80"
+        ),
+        secondary: cn(
+          "border border-border-foreground dark:border-none",
+          "bg-white text-gray-800 hover:bg-gray-100",
+          "dark:bg-[#3b3f5c] dark:text-primary-foreground",
+          "dark:hover:text-primary-foreground/80 dark:hover:bg-[#3b3f5c]/70"
+        )
       }
     }
   });
