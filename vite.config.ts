184806e52cea8111f77f81c360773940238d4308
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2024-11-30 13:13:18 +0100

    Add vite bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { flatRoutes } from "remix-flat-routes";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 installGlobals();
@@ -21,6 +22,7 @@ export default defineConfig({
         v3_singleFetch: true,
       },
     }),
+    visualizer({ emitFile: true }),
   ],
   ssr: {
     noExternal: ["remix-utils"],
