30ae942613e42e9e4cf048a8bd8883205ef105dd
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2025-01-11 10:45:22 +0100

    Tv style combobox

--- app/components/shared/form/combobox.tsx
+++ app/components/shared/form/combobox.tsx
@@ -3,6 +3,7 @@
 //
 
 import React from "react";
+import { useId } from "react";
 import {
   ComboBox as AriaComboBox,
   Button,
@@ -21,9 +22,74 @@ import { cn } from "#app/utils/lib/cn";
 
 const tvo = tv({
   variants: {
+    container: {
+      stacked: "group mb-5",
+      ifta: "group relative mb-5",
+    },
+    group: {
+      stacked: "",
+      ifta: cn(
+        "flex relative rounded-sm focus-within:ring-0",
+        "bg-input hover:bg-input-hover",
+      ),
+    },
+    label: {
+      stacked: cn("block pl-1 mb-1.5 font-medium text-md"),
+      ifta: cn(
+        "absolute text-sm text-secondary-foreground select-none",
+        "pl-[9px] top-6", // alignment
+        "duration-300 transform -translate-y-4 scale-75 z-10 origin-[0] start-2.5",
+        "peer-focus:text-blue-600 peer-focus:dark:text-blue-500",
+        "peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0",
+        "peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto",
+        "group-data-[invalid=true]:text-red-500 group-data-[invalid=true]:peer-focus:text-red-500",
+      ),
+    },
+    input: {
+      stacked: cn(
+        "text-sm text-primary-foreground",
+        "block w-full rounded-md px-3 py-2.5",
+        "placeholder:opacity-80 disabled:pointer-events-none disabled:opacity-50",
+        "focus:border-ring focus:ring-0",
+        "border border-border-foreground",
+        "bg-input",
+        "border-none focus:ring-0 outline-none w-full rounded-md",
+        "shadow-[inset_0_0_0_1px_#d4d4d8] hover:shadow-[inset_0_0_0_1px_#a1a1aa] group-focus-within:shadow-[inset_0_0_0_2px_#3b82f6_!important]",
+        "placeholder:text-[#dfdfdf] group-focus-within:placeholder:text-opacity-0",
+      ),
+      ifta: cn(
+        "peer block relative rounded-sm px-2.5 pb-2.5 pt-8 w-full appearance-none focus:outline-none focus:ring-0",
+        "text-sm text-primary-foreground",
+        "pl-[16px] focus:pl-[13px]", // alignment
+        "bg-input hover:bg-input-hover",
+        "border border-border-foreground",
+        "rounded-sm border-r-0 rounded-r-none",
+        "focus:border-l-4 focus:border-border-foreground focus:border-l-blue-500",
+        "focus:border-l-4 focus:border-border-foreground focus:border-l-blue-500",
+        "group-data-[invalid=true]:border-l-4 group-data-[invalid=true]:border-l-red-500",
+        "group-data-[invalid=true]:pl-[13px]",
+      ),
+    },
+    chevron: {
+      stacked: "",
+      ifta: cn(
+        "flex items-center rounded-r-lg px-5 transition",
+        "border border-border-foreground",
+        "text-secondary-foreground hover:text-primary-foreground",
+        "rounded-sm rounded-l-none",
+      ),
+    },
     fieldError: {
-      stacked: "block pt-1 text-sm text-red-500 ml-2",
-      ifta: "pt-1 text-sm text-red-500 ml-2",
+      stacked: "block pt-1 text-[90%] text-red-500 ml-1",
+      ifta: cn("pt-0.5 text-[85%] text-red-500"),
+    },
+    popover: {
+      stacked: "",
+      ifta: cn(
+        "max-h-60 w-[--trigger-width] overflow-auto rounded-md",
+        "shadow-lg ring-1 ring-black/5",
+        "border border-border-foreground bg-foreground",
+      ),
     },
   },
 });
@@ -43,6 +109,8 @@ export const ComboBox = <T extends object>({
   children,
   ...rest
 }: MyComboBoxProps<T>) => {
+  const id = useId();
+
   return (
     <AriaComboBox
       validationBehavior="aria"
@@ -50,28 +118,23 @@ export const ComboBox = <T extends object>({
       className={"group flex w-full flex-col gap-1 mb-5"}
       aria-label={ariaLabel}
     >
-      <Group
-        className={cn(
-          "flex rounded-lg focus-within:ring-0",
-          "focus-within:border-gray-300 dark:focus-within:border-blue-600",
-          "border border-border-foreground bg-input",
-        )}
-      >
+      <Group className={tvo({ group: "ifta" })}>
         <Input
-          className={cn(
-            "w-full flex-1 rounded-lg border-none px-3 py-2 leading-5 outline-none focus:ring-0",
-            "placeholder:opacity-80 hover:cursor-pointer disabled:pointer-events-none disabled:opacity-50",
-            "bg-input text-sm font-normal text-primary-foreground",
-          )}
+          id={`input${id}`}
+          aria-labelledby={`label${id}`}
+          placeholder=" "
+          className={cn(tvo({ input: "ifta" }))}
         />
 
-        <Button
-          className={cn(
-            "flex items-center rounded-r-lg border-0 border-l border-solid px-3 transition",
-            "border-border-foreground",
-            "bg-transparent text-secondary-foreground hover:text-primary-foreground",
-          )}
+        <label
+          id={`label${id}`}
+          htmlFor={`input${id}`}
+          className={cn(tvo({ label: "ifta" }))}
         >
+          Region
+        </label>
+
+        <Button className={tvo({ chevron: "ifta" })}>
           {/* {isOpen ? <>&#8593;</> : <>&#8595;</>} */}
           <Icon
             name="chevron-down"
@@ -86,18 +149,12 @@ export const ComboBox = <T extends object>({
       </Group>
 
       {description && <Text slot="description">{description}</Text>}
-      <FieldError className={tvo({ fieldError: "stacked" })}>
+      <FieldError className={tvo({ fieldError: "ifta" })}>
         {errorMessage}
       </FieldError>
 
       {/* Popover holding the items */}
-      <Popover
-        className={cn(
-          "max-h-60 w-[--trigger-width] overflow-auto rounded-md",
-          "shadow-lg ring-1 ring-black/5",
-          "border border-border-foreground bg-foreground",
-        )}
-      >
+      <Popover className={tvo({ popover: "ifta" })}>
         <ListBox>{children}</ListBox>
       </Popover>
     </AriaComboBox>
