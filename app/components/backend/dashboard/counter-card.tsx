56cf6404dfc82dc7879e5b8a25c116612c6da246
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2025-01-23 13:08:35 +0100

    Style dashboard tiles

--- app/components/backend/dashboard/counter-card.tsx
+++ app/components/backend/dashboard/counter-card.tsx
@@ -18,24 +18,35 @@ export function DashboardCounterCard({
   return (
     <div
       className={cn(
-        "rounded-md border border-border-foreground bg-foreground p-4 shadow-sm sm:p-5 dark:border-none",
+        "bg-foreground rounded-sm shadow-sm",
+        "border border-border-foreground dark:border-none",
+        "p-3 sm:p-5 ",
         className,
       )}
       {...rest}
     >
-      <div className="sm:flex sm:gap-x-3">
-        {/* Asterix class applies to svg child element */}
-        <span className="mb-2 size-6 shrink-0 text-secondary-foreground *:size-6 sm:order-2 sm:mb-0">
+      <div className="flex gap-x-3">
+        <span
+          className={cn(
+            "shrink-0 order-2",
+            "text-secondary-foreground mb-0",
+            "*:size-5 sm:*:size-6", // these asterisk classes are applied to the SVG child element
+          )}
+        >
           {icon}
         </span>
 
         <div className="grow space-y-1 sm:order-1">
-          <h2 className="text-sm text-secondary-foreground sm:mb-3">{title}</h2>
+          <h2 className="text-sm text-secondary-foreground mb-2 sm:mb-3">
+            {title}
+          </h2>
           <p className="text-lg font-medium text-primary-foreground md:text-xl">
             {count}
           </p>
         </div>
       </div>
+
+      {/* End of Container */}
     </div>
   );
 }
