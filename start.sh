1f4d6a5f75b58bc7018f25c4051df233d5982175
Pinche Madre <170645749+pinche62373@users.noreply.github.com>
2025-02-02 22:07:14 +0100

    done

--- start.sh
+++ start.sh
@@ -5,8 +5,6 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-# woo
-
 # allocate swap space
 fallocate -l 512M /swapfile
 chmod 0600 /swapfile
